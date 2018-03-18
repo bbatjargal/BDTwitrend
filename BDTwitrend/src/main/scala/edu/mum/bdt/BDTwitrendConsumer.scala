9edb71dc4dc0d633cd9642252cd3ad73113650c2
Batjargal B <~b.batja@gmail.com>
2018-03-17 23:21:43 -0500

    updated source code and added zeppelin json file

---
+++ BDTwitrend/src/main/scala/edu/mum/bdt/BDTwitrendConsumer.scala
@@ -0,0 +1,216 @@
+package edu.mum.bdt
+import org.apache.spark.SparkConf
+import org.apache.log4j.{Level, Logger}
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+
+
+import org.apache.spark.sql.execution.datasources.hbase._
+import org.apache.spark.sql._
+import org.apache.spark.sql.SparkSession
+
+
+import java.util.UUID
+import kafka.serializer.StringDecoder
+
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.SparkConf
+
+
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{TableName, HBaseConfiguration}
+import org.apache.hadoop.hbase.client.{ HBaseAdmin, Put, HTable } 
+import org.apache.hadoop.hbase.{ HBaseConfiguration, HTableDescriptor, HColumnDescriptor }
+
+object BDTwitrendConsumer {
+  def main(args: Array[String]) = {
+
+    if (args.length != 2) {
+      System.err.println(s"""
+        |Usage: BDTwitrendConsumer <brokers> <topics>
+        |  <brokers> is a list of one or more Kafka brokers
+        |  <topics> is a list of one or more kafka topics to consume from
+        |
+        """.stripMargin)
+      System.exit(1)
+    }
+    
+        
+    val Array(brokers, topics) = args
+    
+    // Create context with 2 second batch interval
+    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount").setMaster("local[2]").set("spark.executor.memory","1g");
+    val ssc = new StreamingContext(sparkConf, Seconds(6))
+    
+    //val sc = ssc.sparkContext
+    //sc.setLogLevel("ERROR")
+
+    //For hbase
+    //val sc = new SparkContext(sparkConf)
+    
+    try{
+      /*val spark = SparkSession
+                    .builder()
+                    .appName("Spark SQL basic example")
+                    .config("spark.some.config.option", "some-value")
+                    .getOrCreate()
+                    
+      import spark.implicits._ */
+
+      
+      //var hashTagCounts = loadHashTagFromHBase(spark)
+
+      // Create direct kafka stream with brokers and topics
+      val topicsSet = topics.split(",").toSet
+      
+      val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+      
+      val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+        ssc, kafkaParams, topicsSet)
+  
+      // Get the lines, split them into words, count the words and print
+      //val words = messages.map(_._2).flatMap(_.split(" "))
+      //val wordCounts = messages.map(x => (x, 1)).reduceByKey(_ + _)
+      
+      //wordCounts.print()
+  
+      //hashTagCounts.
+      
+      //HBASE
+      val updateFunc = (values: Seq[Int], state: Option[Int]) => { 
+        val currentCount = values.sum       
+        val previousCount = state.getOrElse(0)         
+        val updatedSum = currentCount+previousCount         
+        Some(updatedSum)       
+      }
+      
+      
+      val columnFamily = "Word"
+    
+      //Defining a check point directory for performing stateful operations
+ 
+      ssc.checkpoint("hdfs://localhost:9000/WordCount_checkpoint") 
+      //val cnt = wordCounts.updateStateByKey(updateFunc)  //.map(x => (x, 1)).reduceByKey(_ + _)
+
+
+      val Hbase_inset = messages.foreachRDD(rdd => if (!rdd.isEmpty()) rdd.foreach(toHBase(_)))
+      
+      // Start the computation
+      ssc.start()
+    
+
+      ssc.awaitTermination()
+    
+      
+    } finally {
+    }
+    
+  }
+  
+  def toHBase(row: (_, _)) { 
+
+    val delimiter= "º¿"
+    val hConf = new HBaseConfiguration() 
+    //hConf.set("hbase.zookeeper.quorum", "localhost:2181") 
+    val hTable = new HTable(hConf, "tblTweet") 
+    
+    //hTable.getScanner(x$1)
+    //val tableDescription = new HTableDescriptor(tableName) 
+    //tableDescription.addFamily(new HColumnDescriptor("Details".getBytes()))
+     
+    val data = row._2.toString()
+    val datas = data.split(delimiter)
+    
+    val hastags = datas(0)
+    val text = datas(1)
+    val latitude = datas(2)
+    val longitude = datas(3)
+    val createdAt = datas(4)
+    val user = datas(5)
+				
+    val thePut = new Put(Bytes.toBytes(row._1.toString())) 
+    thePut.add(Bytes.toBytes("Tweet"), Bytes.toBytes("Text"), Bytes.toBytes(text)) 
+    thePut.add(Bytes.toBytes("Tweet"), Bytes.toBytes("Hastags"), Bytes.toBytes(hastags)) 
+    thePut.add(Bytes.toBytes("Tweet"), Bytes.toBytes("Created"), Bytes.toBytes(createdAt)) 
+    thePut.add(Bytes.toBytes("Tweet"), Bytes.toBytes("User"), Bytes.toBytes(user)) 
+    thePut.add(Bytes.toBytes("Geo"), Bytes.toBytes("Latitude"), Bytes.toBytes(latitude)) 
+    thePut.add(Bytes.toBytes("Geo"), Bytes.toBytes("Longitude"), Bytes.toBytes(longitude)) 
+    
+    
+    hTable.put(thePut)
+    
+    
+    val arrTags = hastags.split(" ")
+    
+    val tblHashTag = new HTable(hConf, "tblHashTag") 
+    for( tag <- arrTags){
+      if(tag != "" && tag != "null") {
+        val putHashTag = new Put(Bytes.toBytes(UUID.randomUUID().toString())) 
+        putHashTag.add(Bytes.toBytes("Info"), Bytes.toBytes("HashTag"), Bytes.toBytes(tag)) 
+        putHashTag.add(Bytes.toBytes("Info"), Bytes.toBytes("Created"), Bytes.toBytes(createdAt))  
+        tblHashTag.put(putHashTag)      
+      }
+    }
+    
+  }
+  
+}
+
+/*
+ * 
+ * 
+  
+  def loadHashTagFromHBase(sqlContext: SparkSession) : DataFrame = {
+
+    def catalog = s"""{
+      |"table":{"namespace":"default", "name":"tblTweetHashTag"},
+      |"rowkey":"key",
+      |"columns":{
+        |"col0":{"cf":"rowkey", "col":"key", "type":"string"},
+        |"col1":{"cf":"HashTagCount", "col":"HashTag", "type":"string"},
+        |"col2":{"cf":"HashTagCount", "col":"Count", "type":"string"}
+      |}
+    |}""".stripMargin
+    
+    def withCatalog(cat: String): DataFrame = {
+      sqlContext
+      .read
+      .options(Map(HBaseTableCatalog.tableCatalog->cat))
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .load()
+    }
+    
+    val df = withCatalog(catalog)
+    //val s = df.select("col1", "col2")
+    df.registerTempTable("tblTweetHashTag")
+    //return 
+    return sqlContext.sql("select col0, col2 from tblTweetHashTag") //.show
+    //return s
+    
+  }
+     val Array(input, output) = args
+    
+    //Start the Spark context
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+
+    //Read some example file to a test RDD
+    val test = sc.textFile(input)
+
+    test.flatMap { line => //for each line
+      line.split(" ") //split the line in word by word.
+    }
+      .map { word => //for each word
+        (word, 1) //Return a key/value tuple, with the word as key and 1 as value
+      }
+      .reduceByKey(_ + _) //Sum all of the value with same key
+      .saveAsTextFile(output) //Save to a text file
+
+    //Stop the Spark context
+    sc.stop
+  
+ * */
--- BDTwitrendProducer/src/main/java/edu/mum/bdt/java/BDTwitrendProducer.java
+++ BDTwitrendProducer/src/main/java/edu/mum/bdt/java/BDTwitrendProducer.java
@@ -1,11 +1,18 @@
 package edu.mum.bdt.java;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.StringJoiner;
+import java.util.UUID;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import twitter4j.FilterQuery;
 import twitter4j.HashtagEntity;
@@ -17,22 +24,13 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-/**
- * A Kafka Producer that gets tweets on certain keywords
- * from twitter datasource and publishes to a kafka topic
- * 
- * Arguments: <comsumerKey> <consumerSecret> <accessToken> <accessTokenSecret> <topic-name> <keyword_1> ... <keyword_n>
+/** 
+ * Arguments: <comsumerKey> <consumerSecret> <accessToken> <accessTokenSecret> <topic-name>
  * <comsumerKey>	- Twitter consumer key 
  * <consumerSecret>  	- Twitter consumer secret
  * <accessToken>	- Twitter access token
  * <accessTokenSecret>	- Twitter access token secret
  * <topic-name>		- The kafka topic to subscribe to
- * <keyword_1>		- The keyword to filter tweets
- * <keyword_n>		- Any number of keywords to filter tweets
- * 
- * More discussion at stdatalabs.blogspot.com
- * 
- * @author Sachin Thirumala
  */
 
 public class BDTwitrendProducer {
@@ -41,17 +39,22 @@ public class BDTwitrendProducer {
 
 		if (args.length < 4) {
 			System.out.println(
-					"Usage: BDTwitrendProducer <twitter-consumer-key> <twitter-consumer-secret> <twitter-access-token> <twitter-access-token-secret> <topic-name> <twitter-search-keywords>");
+					"Usage: BDTwitrendProducer <twitter-consumer-key> <twitter-consumer-secret> <twitter-access-token> <twitter-access-token-secret> <topic-name>");
 			return;
 		}
+		//Logger rootLogger = Logger.getRootLogger();
+		//rootLogger.setLevel(Level.ERROR);
+
+//Logger.getLogger("org").setLevel(Level.OFF);
+//Logger.getLogger("akka").setLevel(Level.OFF);
 
 		String consumerKey = args[0].toString();
 		String consumerSecret = args[1].toString();
 		String accessToken = args[2].toString();
 		String accessTokenSecret = args[3].toString();
 		String topicName = args[4].toString();
-		String[] arguments = args.clone();
-		String[] keyWords = Arrays.copyOfRange(arguments, 5, arguments.length);
+		//String[] arguments = args.clone();
+		//String[] keyWords = Arrays.copyOfRange(arguments, 5, arguments.length);
 
 		// Set twitter oAuth tokens in the configuration
 		ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -91,12 +94,15 @@ public class BDTwitrendProducer {
 		twitterStream.addListener(listener);
 
 		// Filter keywords
-		FilterQuery query = new FilterQuery().track(keyWords);
-		twitterStream.filter(query);
+		//FilterQuery query = new FilterQuery().track(new String[] {""});
+		//twitterStream.filter(query);
+		
+		twitterStream.sample("en");
 
 		// Thread.sleep(5000);
 
 		// Add Kafka producer config settings
+		
 		Properties props = new Properties();
 		props.put("metadata.broker.list", "localhost:9092");
 		props.put("bootstrap.servers", "localhost:9092");
@@ -110,25 +116,53 @@ public class BDTwitrendProducer {
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 		Producer<String, String> producer = new KafkaProducer<String, String>(props);
-		int i = 0;
-		int j = 0;
 
 		// poll for new tweets in the queue. If new tweets are added, send them
 		// to the topic
+		StringBuilder sb;
+		String delimiter = "º¿";
+		
+		Locale dateLocale = Locale.US;
+		SimpleDateFormat outFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", dateLocale);
+		
 		while (true) {
+			
 			Status ret = queue.poll();
+			
+			sb = new StringBuilder();			
+			
 			//ret.
 			if (ret == null) {
 				Thread.sleep(100);
 				// i++;
 			} else {
+				StringJoiner hashTags = new StringJoiner(" ");
 				for (HashtagEntity hashtage : ret.getHashtagEntities()) {
-					System.out.println("Tweet:" + ret);
-					System.out.println("Hashtag: " + hashtage.getText());
-					// producer.send(new ProducerRecord<String, String>(
-					// topicName, Integer.toString(j++), hashtage.getText()));
-					producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(j++), ret.getText()));
+					hashTags.add(hashtage.getText());
 				}
+
+				sb.append(hashTags.toString()).append(delimiter);
+				sb.append(ret.getText()).append(delimiter);
+				if(ret.getGeoLocation() != null) {
+					sb.append(ret.getGeoLocation().getLatitude()).append(delimiter);
+					sb.append(ret.getGeoLocation().getLongitude()).append(delimiter);
+				}else {
+					sb.append("").append(delimiter);
+					sb.append("").append(delimiter);
+				}
+				if(ret.getUser() != null) {
+					sb.append(outFormat.format(ret.getUser().getCreatedAt())).append(delimiter);
+					sb.append(ret.getUser().getName());
+				}else {
+					sb.append("").append(delimiter).append(delimiter);
+					sb.append("").append(delimiter);
+				}
+				
+
+				System.out.println("Data: " + sb.toString());
+				producer.send(new ProducerRecord<String, String>(topicName, UUID.randomUUID().toString(), sb.toString() ));
+				
+				//new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date())
 			}
 		}
 		// producer.close();
